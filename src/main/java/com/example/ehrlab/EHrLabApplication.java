b9258f6d79912fa25d04ea249813f13c9b9f7c2b
gukin-han <gukin.dev@gmail.com>
2025-12-10 10:58:17 +0900

    first commit

---
+++ src/main/java/com/example/ehrlab/EHrLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehrlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHrLabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EHrLabApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/ehrlab/EHrLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehrlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHrLabApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
